db74d5223e33234315764464a63492055df19936
John Balke <john.balke@gmail.com>
2019-10-10 15:22:24 +0100

    Exercise 3.8

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./api");
+const morgan = require("morgan");
 const data = require("./data.json");
 
 app.use(express.json());
+
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan((tokens, req, res) => {
+    let output = [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms"
+    ];
+
+    if (req.method === "POST") {
+      output = [...output, tokens.body(req, res)];
+    }
+
+    return output.join(" ");
+  })
+);
 
 app.use("/api", apiRouter(data));
 
