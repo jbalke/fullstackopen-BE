934b182fda8a4aea7bf539ebf46c56617f17193e
John Balke <john.balke@gmail.com>
2019-10-23 20:36:54 +0100

    fixed monodb findOneAndModify deprecation warning

--- src/services/db.js
+++ src/services/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Person = require("../models/person");
 
+mongoose.set("useFindAndModify", false);
 const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
 
 console.log("connecting to", url);
